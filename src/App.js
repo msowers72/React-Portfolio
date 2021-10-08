bffeefbfee7179c455f5c65947007f8ac0f08be5
Mike Sowers <msowers72@yahoo.com>
2021-10-08 18:25:18 -0400

    added styling and completed the nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import React, { useState } from "react";
+
 import "./App.css";
 import Navbar from "./componets/Navbar";
 import Home from "./pages/Home";
+import About from "./componets/About";
+import Contact from "./componets/Contact";
+import Portfolio from "./componets/Portfolio";
+import Resume from "./componets/Resume";
 
 function App() {
+  const [ currentPage, setCurrentPage ] = useState("home");
+
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <Home></Home>;
+    }
+    if (currentPage === "About") {
+      return <About></About>;
+    }
+    if (currentPage === "Resume") {
+      return <Resume></Resume>;
+    }
+    if (currentPage === "Contact") {
+      return <Contact></Contact>;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio></Portfolio>;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <Home></Home>
+      <Navbar
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      ></Navbar>
+
+      {renderPage()}
     </div>
   );
 }
--- src/componets/Navbar.js
+++ src/componets/Navbar.js
@@ -1,22 +1,62 @@
 import React from "react";
 
-export default function Navbar() {
-  return (
-    <div>
-      <div className="navbar">
-        <a className="nave-link" href="">
-          Home
-        </a>
-        <a className="nave-link" href="">
-          About
-        </a>
-        <a className="nave-link" href="">
-          Resume
-        </a>
-        <a className="nave-link" href="">
-          Contact
-        </a>
-      </div>
-    </div>
-  );
+export default function Navbar({ currentPage, handlePageChange }) {
+	return (
+		<div>
+			<div className="navbar">
+				<a
+					// if this is the current page, set to active link otherwise just be a nav-link
+					className={
+						currentPage === "Home" ? "nav-link active" : "nav-link"
+					}
+					href="#home"
+					onClick={() => handlePageChange("Home")}
+				>
+					Home
+				</a>
+				<a
+					className={
+						currentPage === "About" ? "nav-link active" : "nav-link"
+					}
+					href="#about"
+					onClick={() => handlePageChange("About")}
+				>
+					About
+				</a>
+				<a
+					className={
+						currentPage === "Portfolio"
+							? "nav-link active"
+							: "nav-link"
+					}
+					href="#portfolio"
+					onClick={() => handlePageChange("Portfolio")}
+				>
+					Portfolio
+				</a>
+				<a
+					className={
+						currentPage === "Resume"
+							? "nav-link active"
+							: "nav-link"
+					}
+					href="#resume"
+					onClick={() => handlePageChange("Resume")}
+				>
+					Resume
+				</a>
+				<a
+					className={
+						currentPage === "Contact"
+							? "nav-link active"
+							: "nav-link"
+					}
+					href="#contact"
+					onClick={() => handlePageChange("Contact")}
+				>
+					Contact
+				</a>
+			</div>
+		</div>
+	);
 }
