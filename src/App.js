c6fc498923c84b90163beba8552d1f21452e9c51
Mike Sowers <msowers72@yahoo.com>
2021-10-08 17:25:53 -0400

    loaded componet files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./componets/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-     
+      <Navbar></Navbar>
+      <Home></Home>
     </div>
   );
 }
