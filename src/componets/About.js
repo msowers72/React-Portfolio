8ab4fd09b6b02b026ed5611920adda8102abda72
Mike Sowers <msowers72@yahoo.com>
2021-10-08 19:02:43 -0400

    updated contact and about

--- src/componets/About.js
+++ src/componets/About.js
@@ -4,6 +4,11 @@ export default function About() {
   return (
     <div>
 			<div className="px-4 py-5 my-5 text-center">
+        <img 
+        className="d-block mx-auto mb-4 profileImage"
+        src="img/mike.jpg"
+         alt=""
+          />
 				<h1 className="display-5 fw-bold">About</h1>
 				<div className="col-lg-6 mx-auto">
 					<p>
--- src/componets/Contact.js
+++ src/componets/Contact.js
@@ -2,8 +2,14 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Page!</h1>
+    <div className="d-flex justify-content-center">
+      <div className="col-12 col-md-6">
+        <form className="needs-validation" action="POST">
+          <input className="form-control" type="text" placeholder="Name" />
+          <input className="form-control" type="text" placeholder="Email" />
+          <input className="form-control" type="text" placeholder="Message" />
+        </form>
+      </div>
     </div>
   );
 }
